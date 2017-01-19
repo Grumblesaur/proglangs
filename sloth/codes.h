55f4bc11998f5489a3fefc78844a4cb0cdf3dec0
James Murphy <james.f.murphy.one@gmail.com>
2017-01-19 16:03:04 -0500

    broke codes out into separate header file

---
+++ sloth/codes.h
@@ -0,0 +1,46 @@
+#ifndef CODES_SL
+#define CODES_SL
+
+/* Data Codes */
+#define IDENT 100
+#define VALUE 101
+
+/* Arithmetic operators */
+#define PLUS  102
+#define MINUS 103
+#define SLASH 104
+#define SPLAT 105
+
+/* Comparison Operators */
+#define LTHAN 106
+#define GTHAN 107
+#define LTEQL 108
+#define GTEQL 109
+#define EQUAL 110
+#define NOTEQ 111
+
+/* Logical Operators */
+#define AND   112
+#define OR    113
+#define BANG  114
+
+/* Structural Components */
+#define SCOLN 115
+#define SETEQ 116
+#define OPPAR 117
+#define CLPAR 118
+
+/* Control Flow */
+#define START 119
+#define END   120
+#define IF    121
+#define THEN  122
+#define ELSE  123
+#define WHILE 124
+#define DO    125
+
+/* Input / Output */
+#define PRINT 126
+#define INPUT 127
+
+#endif
